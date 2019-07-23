950eb8add9c7e5642343ff2d9aa4a93048eda7c6
Erick Takeshi <erick.tmr@outlook.com>
2019-07-23 18:55:03 -0300

    transfers controller

---
+++ app/controllers/transfers_controller.rb
@@ -0,0 +1,23 @@
+class TransfersController < ApplicationController
+  def create
+    command = CreateTransfer.call(
+      value: transfer_params[:value],
+      source_id: transfer_params[:source_id],
+      destination_id: transfer_params[:destination_id]
+    )
+
+    if command.success?
+      render json: command.result
+    else
+      render json: { errors: command.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def transfer_params
+    params
+      .require(:transfer)
+      .permit(:value, :destination_id, :source_id)
+  end
+end
