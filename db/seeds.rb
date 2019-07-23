391bb9a5ee2def68901c54cdbe37ff9d4dc4bbc2
Erick Takeshi <erick.tmr@outlook.com>
2019-07-23 08:42:41 -0300

    add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+## Users
+
+user_params = [
+  { first_name: 'Erick', last_name: 'Takeshi', email: 'erick@email.com' },
+  { first_name: 'Jãozim', last_name: 'Brabo', email: 'jaozim@email.com' },
+  { first_name: 'Luizodia', last_name: 'Myth', email: 'themyth@email.com' }
+]
+
+user_ids = []
+
+user_params.each do |params|
+  user_ids.push(User.create!(params).id)
+end
+
+account_params = [
+  { balance: Money.new(10000) },
+  { balance: Money.new(1000000) },
+  { balance: Money.new(20000) }
+]
+
+account_params.each_with_index do |params, index|
+  Account.create!(params.merge(user_id: user_ids[index]))
+end
