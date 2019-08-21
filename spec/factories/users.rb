e183aa0a768eba06d2989cae9430bd2f926658c5
Erick Takeshi <erick.tmr@outlook.com>
2019-08-21 16:11:57 -0300

    fix factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,6 +18,6 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { Faker::Internet.email("#{first_name} #{last_name}") }
+    email { Faker::Internet.email(name: "#{first_name} #{last_name}", separators: '.') }
   end
 end
